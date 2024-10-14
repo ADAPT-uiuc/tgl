994b86133341aad6cbfd8353298edb18a744bcdb
wy-go <wanyugogo@gmail.com>
2024-10-13 19:57:35 -0500

    Add 8-gpu exp

---
+++ exp/asplos-slurm-8.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sbatch /u/wanyu/github/tgl/scripts/run-exp-slurm-#8-tgl-netflix.sh
+sbatch /u/wanyu/github/tgl/scripts/run-exp-slurm-#8-tgl-stack-overflow.sh
+sbatch /u/wanyu/github/tgl/scripts/run-exp-slurm-#8-tgl-gdelt.sh
