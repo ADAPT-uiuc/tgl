ced115ec695b834dac21de560523c36db571f03a
wy-go <wanyugogo@gmail.com>
2024-10-13 20:00:23 -0500

    Update scalability surm script

--- exp/asplos-slurm.sh
+++ exp/asplos-slurm.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-srun /u/wanyu/github/tgl/scripts/run-exp-slurm*
+sbatch /u/wanyu/github/tgl/scripts/run-exp-slurm-#1*
+sbatch /u/wanyu/github/tgl/scripts/run-exp-slurm-#2*
+sbatch /u/wanyu/github/tgl/scripts/run-exp-slurm-#4*
