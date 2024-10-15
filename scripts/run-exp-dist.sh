99ee829594e67193e19bf2a8eb51692dfebb2fdd
wy-go <wanyu2@illinois.edu>
2024-10-15 00:17:10 -0500

    Update scalability scriprs

--- scripts/run-exp-dist.sh
+++ scripts/run-exp-dist.sh
@@ -10,8 +10,8 @@ echo "start: $(date)"
 
 num_gpus=4
 
-for data in gdelt mag; do
-  for model in TGN APAN; do
+for data in stack-overflow; do
+  for model in TGAT APAN; do
     echo "tgl $data $model";
     python -m torch.distributed.launch --nproc_per_node=$((num_gpus+1)) \
     train_dist.py --data $data --config "exp/dist/$model.yml" \
@@ -24,24 +24,5 @@ for data in gdelt mag; do
     echo;
   done
 done
-
-
-# for model in APAN JODIE TGAT TGN; do
-#   echo "tgl gdelt $model";
-#   python -m torch.distributed.launch --nproc_per_node=$((num_gpus+1)) \
-#   train_dist.py --data gdelt --config "./config/dist/$model.yml" \
-#   --num_gpus $num_gpus --data_path /shared/;
-#   for rank in $(seq 0 $num_gpus); do
-#     mv out-stats-$rank.csv "out-tgl-gdelt-$model-$rank.csv";
-#   done
-#   echo;
-#   echo "time: $(date)"
-#   echo;
-# done
-
-# model=TGAT
-# python -m torch.distributed.launch --nproc_per_node=$((num_gpus+1)) \
-# train_dist.py --data wiki --config "exp/dist/$model.yml" \
-# --num_gpus $num_gpus --data_path /shared/
 
 echo "end: $(date)"
